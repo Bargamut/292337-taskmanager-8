63721c1f4e6997e75e6c5db870376a625d69dec6
Paul Petrov <paul@bargamut.ru>
2019-03-28 02:46:30 +0300

    Удалить неиспользуемое из моковых данных

--- src/make-data.js
+++ src/make-data.js
@@ -1,5 +1,3 @@
-const cards = [];
-
 export const filters = new Map([
   [
     `all`, {caption: `All`, count: 0}
@@ -18,52 +16,5 @@ export const filters = new Map([
   ],
 ]);
 
-const WEEK_MILLISECONDS = 604800000;
-const arrayTitles = [
-  `Изучить теорию`,
-  `Сделать домашку`,
-  `Пройти интенсив на соточку`
-];
-const arrayTags = [`homework`, `theory`, `practice`, `intensive`, `keks`, `bargamut`, `awesome`, `tibet`];
-
 // Нужны также при постройке шаблона каточки
 export const arrayColors = [`black`, `yellow`, `blue`, `green`, `pink`];
-
-/**
- * @description Генерация объекта карточки
- * @return {Object} Объект карточки
- */
-const generateCardData = () => (
-  {
-    title: arrayTitles[getRandomInt(3)],
-    dueDate: Math.random() > 0.6
-      ? Date.now() + getRandomInt(WEEK_MILLISECONDS, -WEEK_MILLISECONDS)
-      : null,
-    picture: `https://picsum.photos/100/100?r=${Math.random()}`,
-    color: arrayColors[getRandomInt(arrayColors.length)],
-    repeatingDays: {
-      mo: Math.random() > 0.9,
-      tu: Math.random() > 0.9,
-      we: Math.random() > 0.9,
-      th: Math.random() > 0.9,
-      fr: Math.random() > 0.9,
-      sa: Math.random() > 0.9,
-      su: Math.random() > 0.9
-    },
-    isFavorite: Math.random() > 0.5,
-    isDone: Math.random() > 0.5,
-    tags: new Set([
-      arrayTags[getRandomInt(arrayTags.length)],
-      arrayTags[getRandomInt(arrayTags.length)],
-      arrayTags[getRandomInt(arrayTags.length)]
-    ])
-  }
-);
-
-const getRandomInt = (max, min = 0) => Math.floor(Math.random() * (max - min)) + min;
-
-while (cards.length < 7) {
-  cards.push(generateCardData());
-}
-
-export default cards;
